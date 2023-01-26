f6f7e08198a750db5e57daf5e2d9540563e48b9f
Zyphlen Kotani <zkotani@gmail.com>
2023-01-26 14:29:35 -0500

    prototype working

--- test_1.py
+++ test_1.py
@@ -1,6 +1,18 @@
+import sys, getopt
+import tempfile
+import subprocess
 import serial
 
+with open('DLS1_config.txt', 'r', encoding='utf-8') as sw_config:
+    conf_lines = sw_config.read()
+
+conf_list = conf_lines.split('\n')
+print(conf_list)
+
+
 ser = serial.Serial('/dev/ttyUSB0')
-print(ser.name)
-ser.write(b'enable\r\n')
+ser.write(b'enable\r\nconfigure terminal\r\n')
+for line in conf_list:
+    line_b = line.encode()
+    ser.write(line_b + b'\r\n')
 ser.close()
