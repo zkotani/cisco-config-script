b98d2b6525d2a6d5f067d70f0b857517845b6ad4
Zyphlen Kotani <zkotani@gmail.com>
2023-01-27 10:09:54 -0500

    Better handling of options and exceptions.

--- cisco_ios_to_git.py
+++ cisco_ios_to_git.py
@@ -1,6 +1,4 @@
-'''
-Copy Cisco IOS configs to git repo.
-'''
+"Copy Cisco IOS configs to git repo."
 
 import getopt
 import tempfile
@@ -8,16 +6,15 @@ import subprocess
 import sys
 from netmiko import ConnectHandler
 
-# Define global variables
+class MissingMandatoryOpt(Exception):
+    "Raised when one or more mandatory options is missing"
 
-def device_connect(commit_message,
-                    device, device_name,
-                    git_repo, git_user):
+def device_connect(commit_message, device, device_name, git_repo, git_user):
 
     '''
     Connect to device and get device config.
-    Clone git repo in temporary directory,
-        replace files with new config file and push changes back to git repo
+    Clone git repo in temporary directory.
+    Replace files with new config file and push changes back to git repo.
     '''
 
     # Connect to device
@@ -31,39 +28,29 @@ def device_connect(commit_message,
 
     # Create temporary directory and change into it
     temporary_folder = tempfile.TemporaryDirectory()
-    subprocess.call(
-        f'cd {temporary_folder.name}',
-        shell=True)
+    subprocess.call(f'cd {temporary_folder.name}', shell=True)
 
     # Clone Git Repo
     subprocess.call(
         f'cd {temporary_folder.name} && git clone git@github.com:{git_user}/{git_repo}.git .',
         shell=True)
-    subprocess.call(
-        f'rm {device_name}_config.*',
-        shell=True)
+    subprocess.call(f'rm {device_name}_config.*', shell=True)
 
     # Write all config to file
-    with open(f"{temporary_folder.name}/{device_name}_config.txt", 'w', encoding='utf-8') \
-        as outfile:
+    with open(\
+    f"{temporary_folder.name}/{device_name}_config.txt", 'w', encoding='utf-8') as outfile:
         outfile.write(device_config)
 
     # Git commit all changes
-    subprocess.call(
-        f"cd {temporary_folder.name} && git add -A",
-        shell=True)
-    subprocess.call(
-        f"git commit -a -m '{commit_message}' && git push",
-        shell=True)
+    subprocess.call(f"cd {temporary_folder.name} && git add -A", shell=True)
+    subprocess.call(f"git commit -a -m '{commit_message}' && git push", shell=True)
 
     # Delete temporary directory
     temporary_folder.cleanup()
 
 def help_menu():
 
-    '''
-    Stores the help menu text
-    '''
+    "Stores the help menu text"
 
     menu = '''
     Cisco IOS to git repo tool.
@@ -72,24 +59,22 @@ def help_menu():
     
     Options:
     
-    -h,--help=          Print this menu and exit.
-    -n,--name=          Specify the name of the device to be used in <name>_config.txt
-    -d,--devicetype     The type of device being connected to. Default is 'cisco_ios'.
-    -i,--ip=            The IP address of the device you wish to connect to.
-    -u,--user=          The username used to initiate the SSH connection.
-    -p,--pass=          The above user's password.
-    -g,--gituser=       The username belonging to the owner of the repo you would like to save the configuration to.
-    -r,--gitrepo=       The repository you would like to save the configuration to.
-    -c,--commit=        The commit message you would like to use. Wrap multi-word commit messages in single or double quotes.
+    -h  Print this menu and exit.
+    -n  Specify the name of the device to be used in <name>_config.txt
+    -d  The type of device being connected to. Default is 'cisco_ios'.
+    -i  The IP address of the device you wish to connect to.
+    -u  The username used to initiate the SSH connection.
+    -p  The above user's password.
+    -g  The username belonging to the owner of the repo you would like to save the configuration to.
+    -r  The repository you would like to save the configuration to.
+    -c  The commit message you would like to use. Wrap multi-word commit messages in single or double quotes.
     '''
 
     return menu
 
 def main(argv):
 
-    '''
-    Takes in command line options and passes the commands to the device connection function.
-    '''
+    "Takes in command line options and passes the commands to the device connection function."
 
     # Device connection details
     device = {
@@ -103,47 +88,58 @@ def main(argv):
     git_repo = ''
     git_user = ''
     device_name = ''
+    mandatory_options = ['-n', '-d', '-i', '-u', '-p', '-g', '-r', '-c']
+    missing_options = []
 
     # Reads given command-line arguments
+    # If mandatory options are missing or no options are given, exceptions are raised
     try:
-        opts, args = getopt.getopt(argv,'hn:d:i:u:p:g:r:c:',
-                ['help',
-                'name=',
-                'device=',
-                'ip=',
-                'user=','pass=',
-                'gituser=','gitrepo=',
-                'commit='])
-    except getopt.GetoptError:
-        print('That is not a recognized option. Please use -h or --help available options.')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt in ('-h', '--help', ''):
-            print(help_menu())
-            sys.exit()
-        elif opt in ('-n', '--name'):
-            device_name = arg
-        elif opt in ('-d', '--device'):
-            device['device_type'] = arg
-        elif opt in ('-i', '--ip'):
-            device['ip_address'] = arg
-        elif opt in ('-u', '--user'):
-            device['username'] = arg
-        elif opt in ('-p', '--pass'):
-            device['password'] = arg
-        elif opt in ('-g', '--gituser'):
-            git_user = arg
-        elif opt in ('-r', '--gitrepo'):
-            git_repo = arg
-        elif opt in ('-c', '--commit'):
-            commit_message = arg
-    if not opts:
-        print('No options were given. Please use -h or --help to see a list of available options.')
-        sys.exit()
+        opts, args = getopt.getopt(argv,'hn:d:i:u:p:g:r:c:',)
 
-    device_connect(commit_message,
-                    device, device_name,
-                    git_repo, git_user)
+        for opt, arg in opts:
+            if opt in ('-h'):
+                print(help_menu())
+                sys.exit()
+
+        if not opts:
+            print('No options were given. Please use -h to see a list of available options.')
+            sys.exit()
+
+        for option in mandatory_options:
+            if (option not in [opt[0] for opt in opts]):
+                missing_options.append(option)
+
+        if missing_options:
+            raise \
+                MissingMandatoryOpt(f'Mandatory options are missing: {", ".join(missing_options)}')
+
+    except getopt.GetoptError:
+        print('That is not a recognized option. Please use -h to see available options.')
+        sys.exit(2)
+
+    except MissingMandatoryOpt as err:
+        print(f'{err}. Please use -h to see available options.')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ('-n'):
+            device_name = arg
+        elif opt in ('-d'):
+            device['device_type'] = arg
+        elif opt in ('-i'):
+            device['host'] = arg
+        elif opt in ('-u'):
+            device['username'] = arg
+        elif opt in ('-p'):
+            device['password'] = arg
+        elif opt in ('-g'):
+            git_user = arg
+        elif opt in ('-r'):
+            git_repo = arg
+        elif opt in ('-c'):
+            commit_message = arg
+
+    device_connect(commit_message, device, device_name, git_repo, git_user)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
--- git_to_cisco_ios.py
+++ git_to_cisco_ios.py
@@ -1,6 +1,4 @@
-'''
-Copy Cisco IOS configs from git repo.
-'''
+"Copy Cisco IOS configs from git repo."
 
 import getopt
 import subprocess
@@ -8,22 +6,23 @@ import sys
 import tempfile
 import serial
 
+class MissingMandatoryOpt(Exception):
+    "Raised when one or more mandatory options is missing."
+
 def device_connect(device_name, git_repo, git_user, tty_name):
+
     '''
-    Clones the git repository, copies configuration to a list,
-        and writes the confgiuration to the device.
+    Clones the git repository.
+    Copies configuration into a list.
+    Writes the confgiuration to the device.
     '''
 
     # Create temporary directory and change into it
     temporary_folder = tempfile.TemporaryDirectory()
-    subprocess.call(
-        f'cd {temporary_folder.name}',
-        shell=True)
+    subprocess.call(f'cd {temporary_folder.name}', shell=True)
 
     # Clone Git Repo
-    subprocess.call(
-        f'git clone git@github.com:{git_user}/{git_repo}.git .',
-        shell=True)
+    subprocess.call(f'git clone git@github.com:{git_user}/{git_repo}.git .', shell=True)
 
     # Copy the config to a list containing each individual line
     with open(f'{device_name}', 'r', encoding='utf-8') as sw_config:
@@ -35,17 +34,17 @@ def device_connect(device_name, git_repo, git_user, tty_name):
 
     ser = serial.Serial(f'/dev/{tty_name}')
     ser.write(b'enable\r\nconfigure terminal\r\n')
+
     for line in conf_list:
         line_b = line.encode()
         ser.write(line_b + b'\r\n')
+
     ser.close()
 
 
 def help_menu():
 
-    '''
-    Stores the help menu text
-    '''
+    "Stores the help menu text"
 
     menu = '''
     Git repo to Cisco IOS tool.
@@ -54,11 +53,11 @@ def help_menu():
     
     Options:
     
-    -h,--help=          Print this menu and exit.
-    -n,--name=          Specify the name of the device to be used in <name>_config.txt
-    -t,--tty=           The name of the device being used for the serial connection. (e.g. ttyUSB0)
-    -g,--gituser=       The username belonging to the owner of the repo you would like to copy the configuration from.
-    -r,--gitrepo=       The repository you would like to copy the configuration from.
+    -h  Print this menu and exit.
+    -n  Specify the name of the device to be used in <name>_config.txt
+    -t  The name of the device being used for the serial connection. (e.g. ttyUSB0)
+    -g  The username belonging to the owner of the repo you would like to copy the configuration from.
+    -r  The repository you would like to copy the configuration from.
     '''
 
     return menu
@@ -72,32 +71,48 @@ def main(argv):
     git_repo = ''
     git_user = ''
     tty_name = ''
+    mandatory_options = ['-n', '-t', '-g', '-r']
+    missing_options = []
 
     # Reads given command-line arguments
+    # If a mandatory option is missing or none are given, exceptions are raised.
     try:
-        opts, args = getopt.getopt(argv,'hn:t:g:r:',
-                ['help',
-                'name=',
-                'tty=',
-                'gituser=','gitrepo='])
-    except getopt.GetoptError:
-        print('That is not a recognized option. Please use -h or --help available options.')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt in ('-h', '--help', ''):
-            print(help_menu())
+        opts, args = getopt.getopt(argv,'hn:t:g:r:')
+
+        for opt, arg in opts:
+            if opt in ('-h'):
+                print(help_menu())
+                sys.exit()
+
+        if not opts:
+            print('No options were given. Please use -h to see a list of available options.')
             sys.exit()
-        elif opt in ('-n', '--name'):
+
+        for option in mandatory_options:
+            if (option not in [opt[0] for opt in opts]):
+                missing_options.append(option)
+
+        if missing_options:
+            raise \
+                MissingMandatoryOpt(f'Mandatory options are missing: {", ".join(missing_options)}')
+
+    except getopt.GetoptError:
+        print('That is not a recognized option. Please use -h to see a list of available options.')
+        sys.exit(2)
+
+    except MissingMandatoryOpt as err:
+        print(f'{err}. Please use -h to see available options.')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ('-n'):
             device_name = arg
-        elif opt in ('-t', '--tty'):
+        elif opt in ('-t'):
             tty_name = arg
-        elif opt in ('-g', '--gituser'):
+        elif opt in ('-g'):
             git_user = arg
-        elif opt in ('-r', '--gitrepo'):
+        elif opt in ('-r'):
             git_repo = arg
-    if not opts:
-        print('No options were given. Please use -h or --help to see a list of available options.')
-        sys.exit()
 
     device_connect(device_name, git_repo, git_user, tty_name)
 
