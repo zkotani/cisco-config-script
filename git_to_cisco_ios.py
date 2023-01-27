ec3a946e1757e395d98b54a4a827703cba49c107
Zyphlen Kotani <zkotani@gmail.com>
2023-01-27 16:27:29 -0500

    git to cisco over ssh seems to work. need to test in lab

--- git_to_cisco_ios.py
+++ git_to_cisco_ios.py
@@ -4,12 +4,13 @@ import getopt
 import subprocess
 import sys
 import tempfile
+from netmiko import ConnectHandler
 import serial
 
 class MissingMandatoryOpt(Exception):
     "Raised when one or more mandatory options is missing."
 
-def device_connect(device_name, git_repo, git_user, tty_name):
+def device_connect(device_name, git_repo, git_user, tty_name, serial_or_ssh, device):
 
     '''
     Clones the git repository.
@@ -19,27 +20,39 @@ def device_connect(device_name, git_repo, git_user, tty_name):
 
     # Create temporary directory and change into it
     temporary_folder = tempfile.TemporaryDirectory()
-    subprocess.call(f'cd {temporary_folder.name}', shell=True)
+    subprocess.call(
+        f'cd {temporary_folder.name}',
+        shell=True
+    )
 
     # Clone Git Repo
-    subprocess.call(f'git clone git@github.com:{git_user}/{git_repo}.git .', shell=True)
+    subprocess.call(
+        f'cd {temporary_folder.name} && git clone git@github.com:{git_user}/{git_repo}.git .',
+        shell=True
+    )
 
-    # Copy the config to a list containing each individual line
-    with open(f'{device_name}', 'r', encoding='utf-8') as sw_config:
-        conf_lines = sw_config.read()
-    conf_list = conf_lines.split('\n')
+    if serial_or_ssh == 'serial':
+        # Copy the config to a list containing each individual line
+        with open(
+            f'{temporary_folder.name}/{device_name}_config.txt', 'r', encoding='utf-8'
+        ) as sw_config:
+            conf_lines = sw_config.read()
+        conf_list = conf_lines.split('\n')
+
+        ser = serial.Serial(f'/dev/{tty_name}')
+        ser.write(b'enable\r\nconfigure terminal\r\n')
+        for line in conf_list:
+            line_b = line.encode()
+            ser.write(line_b + b'\r\n')
+        ser.close()
+    elif serial_or_ssh == 'ssh':
+        # Connect to device
+        net_connect = ConnectHandler(**device)
+        net_connect.send_config_from_file(f'{temporary_folder.name}/{device_name}_config.txt')
+        net_connect.disconnect()
 
     # Delete temporary directory
     temporary_folder.cleanup()
-
-    ser = serial.Serial(f'/dev/{tty_name}')
-    ser.write(b'enable\r\nconfigure terminal\r\n')
-
-    for line in conf_list:
-        line_b = line.encode()
-        ser.write(line_b + b'\r\n')
-
-    ser.close()
 
 
 def help_menu():
@@ -55,9 +68,16 @@ def help_menu():
     
     -h  Print this menu and exit.
     -n  Specify the name of the device to be used in <name>_config.txt
-    -t  The name of the device being used for the serial connection. (e.g. ttyUSB0)
     -g  The username belonging to the owner of the repo you would like to copy the configuration from.
     -r  The repository you would like to copy the configuration from.
+    -s  Declare whether you're using a serial or SSH connection (optional, default is serial).
+        If using a serial connection:
+            -t  The name of the device being used for the serial connection, e.g. ttyUSB0.
+        If using an SSH connection:
+            -d  The type of device being connected to. Default is 'cisco_ios' (Optional).
+            -i  The IP address of the device you wish to connect to.
+            -u  The username used to initiate the SSH connection.
+            -p  The above user's password.
     '''
 
     return menu
@@ -68,25 +88,47 @@ def main(argv):
     Takes in command line options and passes the commands to the device connection function.
     '''
 
+    # Device connection details
+    device = {
+        'device_type': 'cisco_ios',
+        'host': '',
+        'username': '',
+        'password': ''
+    }
+
     git_repo = ''
     git_user = ''
     tty_name = ''
-    mandatory_options = ['-n', '-t', '-g', '-r']
+    serial_or_ssh = 'serial'
+    mandatory_options = ['-n', '-g', '-r']
     missing_options = []
 
     # Reads given command-line arguments
     # If a mandatory option is missing or none are given, exceptions are raised.
     try:
-        opts, args = getopt.getopt(argv,'hn:t:g:r:')
+        opts, args = getopt.getopt(argv,'hn:g:r:s:t:d:i:u:p:')
 
         for opt, arg in opts:
             if opt in ('-h'):
                 print(help_menu())
                 sys.exit()
+            if opt in ('-s'):
+                if not arg.lower() in ('serial', 'ssh'):
+                    print('When using the -s option, enter either "serial" or "ssh".')
+                    sys.exit(2)
+                else:
+                    serial_or_ssh = arg.lower()
+                    if serial_or_ssh == 'serial':
+                        mandatory_options.append('-t')
+                    elif serial_or_ssh == 'ssh':
+                        new_opts = ['-i', '-u', '-p']
+                        for i in new_opts:
+                            mandatory_options.append(i)
+
 
         if not opts:
             print('No options were given. Please use -h to see a list of available options.')
-            sys.exit()
+            sys.exit(2)
 
         for option in mandatory_options:
             if (option not in [opt[0] for opt in opts]):
@@ -113,8 +155,16 @@ def main(argv):
             git_user = arg
         elif opt in ('-r'):
             git_repo = arg
+        elif opt in ('-d'):
+            device['device_type'] = arg
+        elif opt in ('-i'):
+            device['host'] = arg
+        elif opt in ('-u'):
+            device['username'] = arg
+        elif opt in ('-p'):
+            device['password'] = arg
 
-    device_connect(device_name, git_repo, git_user, tty_name)
+    device_connect(device_name, git_repo, git_user, tty_name, serial_or_ssh, device)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
